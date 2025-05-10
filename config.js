fb19fb569c0600d9d12ac1a3f0b34db5afd61f99
arnaldo-chioquetta2 <arnaldohailou@gmail.com>
2025-05-10 00:54:18 -0300

    Ajustes

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
 const API_BASE_URL = "http://localhost:8001";
+// const API_BASE_URL = "https://cardapiointeligente-api-afc4ctbbdtdbbjf7.brazilsouth-01.azurewebsites.net";
--- sobe.bat
+++ sobe.bat
@@ -1,12 +1,13 @@
 @echo off
+echo 🔗 Repositório remoto configurado:
+git remote -v
+echo.
+git status
 set /p MSG=Digite a mensagem de commit e tecle ENTER: 
 echo 📁 Adicionando arquivos modificados...
 git add .
-REM Commit
 echo 💬 Commitando com a mensagem: "%MSG%"
 git commit -m "%MSG%"
 echo 🚀 Enviando para o GitHub...
 git push origin main
-
-REM caso queira uma pausa no final
-PAUSE
+pause
